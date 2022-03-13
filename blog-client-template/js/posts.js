62479daccef023855e8b62007bafbf73c1b90391
AlexanderWiman <alexander.wiman@cmeducations.se>
2022-03-13 14:41:50 +0100

    fixed clickbait

--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -1,31 +1,64 @@
 window.onload = function() {
-    fetchPosts();
+    fetchPosts ();
 }
 
 
-async function fetchPosts() {
-    try {
-        const response = await fetch('http://localhost:5000/posts')
-        const posts = await response.json();
-        console.log(posts);
 
+    async function fetchPosts() {
+        try {
+            const response = await fetch('http://localhost:5000/posts')
+            const posts = await response.json();
+            
+    
+            let html = ''
+    
+            for (let post of posts) {
+               
+    
+                const tags = post.tags;
+                let tag    = "";
+                let author = "";
+                let title = "";
+                let x = "";
+    
+                // Ändrar till "okänd författare" om post.author är null
+                if (post.author == null) {
+                    author = "Okänd författare"
+                } else {
+                    author = post.author;
+                }
+    
+                // Visar bara taggar som inte är null
+                if (tags != null) {
+                    tag = tags;
+                }
+                
+                if (post.title && post.title.length > 10) {
+                    title = post.title;
+                } else {
 
-        let html = ''
-        for (let post of posts) {
-            html += `
-                <div class="col-sm m-5 p-5 shadow bg-white">
-                    <h2>${post.title}</h2>
-                    <p>${post.author} - ${post.date.substring(0, 10)}</p>
-                    <blockquote class="blockquote fst-italic fw-light">"${post.content.substring(0, 100)}"</blockquote>
-                    <a href="#">Läs mer!</a>
-                </div>
-            `
+                    const anotherResponse = await fetch('https://my-json-server.typicode.com/AlexanderWiman/quotes/quotes', {method: 'get'});
+                    const anotherdata = await anotherResponse.json();
+                    
+                    for (let i = 0; i < anotherdata.length; i++) {
+                        title = anotherdata[Math.floor(Math.random() * anotherdata.length)].quote;  
+                    }
+                }
+    
+                html += `
+                    <div class="col-sm m-5 p-5 shadow bg-white">
+                        <h2>${title}</h2>
+                        <p>${author} - ${post.date.substring(0, 10)}</p>
+                        <blockquote class="blockquote fst-italic fw-light">"${post.content.substring(0, 100)}"</blockquote>
+                        <p>${tag}</p>
+                        <a href="#">Läs mer!</a>
+                    </div>
+                `
+            }
+            document.getElementById('post-list').innerHTML = html;
+          
+        } catch(error) {
+            console.log(error)
         }
-        document.getElementById('post-list').innerHTML = html;
-    } catch(error) {
-        console.log(error)
+    
     }
-
-
- 
-}
