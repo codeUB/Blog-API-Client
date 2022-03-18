db1cbe1241fca6782d48483c497ae733fb37cba3
AlexanderWiman <alexander.wiman@cmeducations.se>
2022-03-18 23:28:57 +0100

    Final touches

--- blog-client-template/js/post.js
+++ blog-client-template/js/post.js
@@ -1,10 +1,17 @@
 window.onload = function() {
-    var url = document.URL;
-    var id = url.substring(url.lastIndexOf('=') + 1);
-    fetchPost(id)
+
+    // Really crappy way to find out post_id and titleId
+    var url      = document.URL;
+    var call     = url.substring(url.indexOf('=') + 1);
+    var titleId  = url.substring(url.indexOf('title=') + 6);
+    let question = "&title=";
+    let trim     = question.concat(titleId);
+    var id       = call.replace(trim, "")
+    
+    fetchPost(id,titleId)
 }
 
-async function fetchPost(id) {
+async function fetchPost(id,titleId) {
     try {
         const response = await fetch('http://localhost:5000/posts/' + id)
         const post = await response.json();
@@ -18,20 +25,22 @@ async function fetchPost(id) {
             let title  = "";
             let text   = "";
 
-            // Ändrar till "okänd författare" om post.author är null
+            // Changes to unknown author if post.authur equals null. Looks better that way.
             if (post.author == null) {
                 author = "Okänd författare"
             } else {
                 author = post.author;
             }
 
-            // Visar bara taggar som inte är null
-            if (tags != null) {
-                tag = tags;
+            // Let´s remove null and empty tags
+            if (post.tags != null && post.tags != "") {
+                tag = "Tags: " + post.tags;   
+            } else {
+                tag = "";
             }
             
-            // Här tar vi bort titlar som är null eller kortare än 10 bokstäver. 
-            // Är titeln kort så hämtar vi en random titel från ett clickbait API.
+            // Here we remove titles that are null or shorter then 10 letters 
+            // If that happens, we fetch a random title from a clickbait API
             if (post.title && post.title.length > 10) {
                 title = post.title;
             } else {
@@ -39,26 +48,31 @@ async function fetchPost(id) {
                 const anotherResponse = await fetch('https://my-json-server.typicode.com/AlexanderWiman/quotes/quotes', {method: 'get'});
                 const anotherdata = await anotherResponse.json();
                 
-                for (let i = 0; i < anotherdata.length; i++) {
-                    title = anotherdata[Math.floor(Math.random() * anotherdata.length)].quote;  
-                }
+                
+                    title = anotherdata[titleId].quote;  
+                
                 }
 
-            // Undviker att visa tomma inlägg, lägger en dummy text istället
+            // We don´t want empty posts, in case we put in a dummy text
             if (post.content == null) {
                 text = "Författaren har inte skrivit någon text. Så vi lägger till den här texten istället, så blir det inte tomt på sidan.";
             } else {
                 text = post.content;
             }
 
+            // Generate a random id, so we can get a random pic
+            let imgId = Math.floor(Math.random() * 1000);
+
             html += `
                 <div class="col-sm m-5 p-5 shadow bg-white">
                     <h2>${title}</h2>
                     <p>${author} - ${post.date.substring(0, 10)}</p>
+                    <img src="https://picsum.photos/id/${imgId}/600/300">
                     <blockquote class="blockquote fst-italic fw-light">"${text}"</blockquote>
                     <p>${tag}</p>
                 </div>
             `
+    
         
         document.getElementById('post-list').innerHTML = html;
       
--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -4,6 +4,7 @@ window.onload = function() {
 
 }
 
+// Here we fetch the tags, for the sidebar
 async function fetchTags(){
     try {
         const data = await fetch('http://localhost:5000/posts')
@@ -13,6 +14,7 @@ async function fetchTags(){
 
         for (let tag of post){
 
+            //We dont want tags that are null or empty
             if(tag.tags != null && tag.tags != ""){
 
                 tag.tags.forEach(myFunction);
@@ -33,77 +35,87 @@ async function fetchTags(){
 
 
 
-    async function fetchPosts() {
-        try {
-            const response = await fetch('http://localhost:5000/posts')
-            const posts = await response.json();
-            
-    
-            let html = ''
+async function fetchPosts() {
+    try {
+        const response = await fetch('http://localhost:5000/posts')
+        const posts = await response.json();
 
-            for (let post of posts) {
-               
-            
-                let tag    = "";
-                let author = "";
-                let title  = "";
-                let text   = "";
-    
-                // Ändrar till "okänd författare" om post.author är null
-                if (post.author == null) {
-                    author = "Okänd författare"
-                } else {
-                    author = post.author;
-                }
 
-                
-                // Här tar vi bort titlar som är null eller kortare än 10 bokstäver. 
-                // Är titeln kort så hämtar vi en random titel från ett clickbait API.
-                if (post.title && post.title.length > 10) {
-                    title = post.title;
-                } else {
+        let html = ''
 
-                    const anotherResponse = await fetch('https://my-json-server.typicode.com/AlexanderWiman/quotes/quotes', {method: 'get'});
-                    const anotherdata = await anotherResponse.json();
-                    
-                    for (let i = 0; i < anotherdata.length; i++) {
-                        title = anotherdata[Math.floor(Math.random() * anotherdata.length)].quote;  
-                    }
-                }
-
-                // Undviker att visa tomma inlägg, lägger en dummy text istället
-                if (post.content == null) {
-                    text = "Författaren har inte skrivit någon text. Så vi lägger till den här texten istället, så blir det inte tomt på sidan.";
-                } else {
-                    text = post.content.substring(0, 100);
-                }
-
-                // Visar bara taggar som inte är null
-                if (post.tags != null || post.tags != "") {
-                    tag = post.tags;
-                } else {
-                    tag = "";
-                }
-
-                // Genererar ett random ID så att vi kan hämta en random bild
-                let imgId = Math.floor(Math.random() * 1000);
-
-                html += `
-                    <div class="col-sm m-5 p-5 shadow bg-white">
-                        <h2>${title}</h2>
-                        <img src="https://picsum.photos/id/${imgId}/600/300">
-                        <p>${author} - ${post.date.substring(0, 10)}</p>
-                        <blockquote class="blockquote fst-italic fw-light">"${text}"</blockquote>
-                        <p>${tag}</p>
-                        <a href="post.html?id=${post._id}">Läs mer!</a>
-                    </div>
-                `
-            }
+    for (let post of posts) {
         
-            document.getElementById('post-list').innerHTML = html;
-          
-        } catch(error) {
-            console.log(error)
+
+        let tag     = "";
+        let author  = "";
+        let title   = "";
+        let text    = "";
+        let titleId = "";
+
+        // Changes to unknown author if post.authur equals null. Looks better that way.
+        if (post.author == null) {
+            author = "Okänd författare"
+        } else {
+            author = post.author;
         }
-    
+
+        
+        // Here we remove titles that are null or shorter then 10 letters 
+        // If that happens, we fetch a random title from a clickbait API
+        if (post.title && post.title.length > 10) {
+            title = post.title;
+        } else {
+
+            const anotherResponse = await fetch('https://my-json-server.typicode.com/AlexanderWiman/quotes/quotes', {method: 'get'});
+            const anotherData = await anotherResponse.json();
+           
+            
+           
+            for (let i = 0; i < anotherData.length; i++) {
+
+                // ################## OBS ##################
+                // Kan göra det så här, men jag vill ha med mig ID´t till post.js
+                // title = anotherData[Math.floor(Math.random() * anotherData.length)].quote;  
+
+                // Så jag gör så här istället
+                titleId = Math.floor(Math.random() * anotherData.length);
+                title = anotherData[titleId].quote;
+            }
+        }
+
+        // We don´t want empty posts, in case we put in a dummy text
+        if (post.content == null) {
+            text = "Författaren har inte skrivit någon text. Så vi lägger till den här texten istället, så blir det inte tomt på sidan.";
+        } else {
+            text = post.content.substring(0, 100);
+        }
+
+        // Let´s remove null and empty tags
+        if (post.tags != null && post.tags != "") {
+            tag = "Tags: " + post.tags;   
+        } else {
+            tag = "";
+        }
+
+        // Generate a random id, so we can get a random pic
+        let imgId = Math.floor(Math.random() * 1000);
+
+        html += `
+            <div class="col m-5 p-5 shadow bg-white">
+                <h2><a href="post.html?id=${post._id}&title=${titleId}">${title}</a></h2>
+                <img src="https://picsum.photos/id/${imgId}/300">
+                <p>${author} - ${post.date.substring(0, 10)}</p>
+                <blockquote class="blockquote fst-italic fw-light">"${text}"</blockquote>
+                <p>${tag}</p>
+                <a href="post.html?id=${post._id}&title=${titleId}">Läs mer!</a>
+            </div>
+        `
     }
+
+    document.getElementById('post-list').innerHTML = html;
+
+    } catch(error) {
+    console.log(error)
+    }
+
+}
